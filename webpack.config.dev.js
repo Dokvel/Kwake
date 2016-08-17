0ca074bf3db61875e8982a5d04c2e5f82f12d239
Dima Mikolyuk <kissmyfullmetalass@gmail.com>
2016-08-17 13:59:21 +0300

    Style Configuration (#2)
    Base style variables, webpack sass configuration and adding additional fonts

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -39,21 +39,37 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: 'style-loader!css-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap!postcss-loader',
+        loader: [
+          'style-loader',
+          'css-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap',
+          'postcss-loader'
+        ],
       }, {
         test: /\.css$/,
         include: /node_modules/,
-        loaders: ['style-loader', 'css-loader'],
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ],
+      }, {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
+          'style-loader',
+          'css-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap',
+          'sass-loader?localIdentName=[name]__[local]__[hash:base64:5]&modules&importLoaders=1&sourceMap',
+          'postcss-loader'
+        ],
       }, {
         test: /\.jsx*$/,
         exclude: [/node_modules/, /.+\.config.js/],
-        loader: 'babel',
+        loader: 'babel'
       }, {
         test: /\.(jpe?g|gif|png|svg)$/i,
-        loader: 'url-loader?limit=10000',
+        loader: 'url-loader?limit=10000'
       }, {
         test: /\.json$/,
-        loader: 'json-loader',
+        loader: 'json-loader'
       },
     ],
   },
