18ff436dbfc06b76c9c540a88a4311a1d3e05fe1
Aleksei Barbarosh <barbarosh@ukr.net>
2016-08-20 13:26:37 +0300

    Tech rout to drop all data. Style fixes.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,5 +5,8 @@ const router = new Router();
 
 // Add a new Post
 router.route('/profile').put(UserController.setupProfile);
+if (process.env.DEMO_MODE) {
+  router.route('/drop_all').get(UserController.dropAll);
+}
 
 export default router;
