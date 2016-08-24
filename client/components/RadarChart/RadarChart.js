c16d042dc9f7800db27347886947e01340ee2258
DimaMklk <kissmyfullmetalass@gmail.com>
2016-08-24 23:46:03 +0300

    RadarChart correction

--- client/components/RadarChart/RadarChart.js
+++ client/components/RadarChart/RadarChart.js
@@ -19,7 +19,7 @@ export default class RadarChart extends Component {
     let talentsObj = _.keyBy(dataTalents, 'key');
     let { talents } = this.props.user;
 
-    let points = [ 5, 4, 3.5, 5, 4.75 ];
+    let points = [ 5,1,3,5,4 ];
     let baseMult = 3;
 
     let subPoints = [];
@@ -30,9 +30,51 @@ export default class RadarChart extends Component {
     for (var i = 0; i < points.length; i++) {
       if (i === (points.length - 1)) {
         subPoint = (points[i] + points[0])/baseMult;
+        // 1 | 5
+        if ((baseMult/1.8 <= subPoint && subPoint <= baseMult/1.5) && (points[i]>baseMult*1.6 || points[0]>baseMult*1.6)) {
+          subPoint = subPoint*(baseMult/3.61);
+        }
+        // 1 | 4
+        if ((baseMult/2.24 <= subPoint && subPoint < baseMult/1.799) && (points[i]>baseMult*1.3 || points[0]>baseMult*1.3)) {
+          subPoint = subPoint*(baseMult/3.7);
+        }
+        // 1 | 3
+        if (baseMult/baseMult < subPoint && subPoint < baseMult/2.24) {
+          subPoint = subPoint*(baseMult/3.16);
+        }
+        // 1 | 2
+        if (baseMult/4.48 <= subPoint && subPoint <= baseMult/baseMult) {
+          subPoint = subPoint*(baseMult/2.5);
+        }
+        // 1 | 1
+        if (subPoint < baseMult/4.48) {
+          subPoint = subPoint*(baseMult/2);
+        }
+
         subPoints.push(subPoint);
       } else {
         subPoint = (points[i] + points[i+1])/baseMult;
+        // 1 | 5
+        if ((baseMult/1.8 <= subPoint && subPoint <= baseMult/1.5) && (points[i+1]>baseMult*1.6 || points[0]>baseMult*1.6)) {
+          subPoint = subPoint*(baseMult/3.61);
+        }
+        // 1 | 4
+        if ((baseMult/2.24 <= subPoint && subPoint < baseMult/1.79) && (points[i+1]>baseMult*1.3 || points[0]>baseMult*1.3)) {
+          subPoint = subPoint*(baseMult/3.7);
+        }
+        // 1 | 3
+        if (baseMult/baseMult < subPoint && subPoint < baseMult/2.24) {
+          subPoint = subPoint*(baseMult/3.16);
+        }
+        // 1 | 2
+        if (baseMult/4.48 <= subPoint && subPoint <= baseMult/baseMult) {
+          subPoint = subPoint*(baseMult/2.5);
+        }
+        // 1 | 1
+        if (subPoint < baseMult/4.48) {
+          subPoint = subPoint*(baseMult/2);
+        }
+
         subPoints.push(subPoint);
       }
     }
