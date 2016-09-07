7a166d06ebf8b79c42cd833c69f5db3f4fda4d0b
Aleksei Barbarosh <barbarosh@ukr.net>
2016-09-07 11:36:53 +0300

    Rove unused code

--- client/modules/Admin/Admin.js
+++ client/modules/Admin/Admin.js
@@ -1,47 +1,9 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-
-// Import Components
-import {} from '../../util/apiCaller';
-
-// Import Actions
-import {} from './AdminActions';
-import callApi from '../../util/apiCaller';
-
-// Import Selectors
-import {} from './AdminReducer';
+import React, { Component } from 'react';
 
 export class AdminApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isMounted: false };
-  }
-
-  componentWillMount() {
-
-  }
-
-  componentDidMount() {
-    this.setState({ isMounted: true }); // eslint-disable-line
-  }
-
   render() {
     return this.props.children;
   }
 }
 
-AdminApp.propTypes = {
-  children: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired,
-};
-
-AdminApp.contextTypes = {
-  router: React.PropTypes.object,
-};
-
-// Retrieve data from store as props
-function mapStateToProps(store) {
-  return {};
-}
-
-export default connect(mapStateToProps)(AdminApp);
+export default AdminApp;
