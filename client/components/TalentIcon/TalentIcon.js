e358cb740fa66fcfba051c9f1fff55bbba5fcbdf
dima.mklk <kissmyfullmetalass@gmail.com>
2016-09-09 14:12:01 +0300

    TalentIcon unselect wave effect
    z-index: -1 for wave div is necessary, but it causes bug. When you select icon, there is some delay before animation starts. If you set wave's z-index to 1, you'll not be able to deselect icon.
    Bugfix will be released as soon as possible

--- client/components/TalentIcon/TalentIcon.js
+++ client/components/TalentIcon/TalentIcon.js
@@ -4,48 +4,66 @@ import Tooltip from '../Tooltip/Tooltip';
 
 import styles from './TalentIcon.scss';
 
-function TalentIcon(props) {
-  let iconWrapper = cn(styles.icon, {
-    [styles.circled]: props.circled && !props.selected,
-    [styles['icon--enabled']]: !!props.onClick
-  });
+export default class TalentIcon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: undefined };
+  }
 
-  let jumpingContainer = cn({
-    [styles.jumpingContainer]: props.selected
-  });
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selected) {
+      this.setState({ selected: 'selected' });
+    } else if (this.props.selected && !nextProps.selected) {
+      this.setState({ selected: 'unselected' });
+    }
+  }
 
-  let wave = cn ({
-    [styles.wave]: props.selected
-  });
+  render() {
+    let iconWrapper = cn(styles.icon, {
+      [styles.circled]: this.props.circled && !this.props.selected,
+      [styles['icon--enabled']]: !!this.props.onClick
+    });
 
-  return (
-    <Tooltip id={props.talentKey} tip={props.tip}>
-      <div className={jumpingContainer}>
-        <div className={wave}></div>
-        <div className={iconWrapper} onClick={props.onClick}>
-          <i className={props.name || "bi_com-group-bubble-a"} aria-hidden="true"/>
+    let jumpingContainer = cn({
+      [styles.jumpingContainer]: this.props.selected
+    });
+
+    let waveT = cn ({
+      [styles.wave]: this.state.selected === 'selected'
+    });
+    let waveF = cn ({
+      [styles.wave]: this.state.selected === 'unselected'
+    });
+
+    return (
+      <Tooltip id={this.props.talentKey} tip={this.props.tip}>
+        <div className={styles.wrapper}>
+          <div className={jumpingContainer}>
+            <div className={iconWrapper} onClick={this.props.onClick}>
+              <i className={this.props.name || "bi_com-group-bubble-a"} aria-hidden="true"/>
+            </div>
+            <div className={styles.liquidContainer}>
+              <div className={styles.mainBall}></div>
+              <div className={styles.ball}></div>
+              <div className={styles.ball}></div>
+              <div className={styles.ball}></div>
+              <div className={styles.ball}></div>
+            </div>
+          </div>
+          <div className={waveT}></div>
+          <div className={waveF}></div>
         </div>
-        <div className={styles.liquidContainer}>
-          <div className={styles.mainBall}></div>
-          <div className={styles.ball}></div>
-          <div className={styles.ball}></div>
-          <div className={styles.ball}></div>
-          <div className={styles.ball}></div>
-          <div className={styles.ball}></div>
-        </div>
-      </div>
 
-      <svg id={styles.svgFilters}>
-        <defs>
-          <filter id="filter">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -10" result="filter" />
-            <feComposite in="SourceGraphic" in2="filter" operator="atop" />
-          </filter>
-        </defs>
-      </svg>
-    </Tooltip>
-  )
+        <svg id={styles.svgFilters}>
+          <defs>
+            <filter id="filter">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -10" result="filter" />
+              <feComposite in="SourceGraphic" in2="filter" operator="atop" />
+            </filter>
+          </defs>
+        </svg>
+      </Tooltip>
+    )
+  }
 }
-
-export default TalentIcon;
